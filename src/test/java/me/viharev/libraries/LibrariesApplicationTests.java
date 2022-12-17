80032c2f7402ba364b6ff488712ec36450e6c459
ViharevN <swix_off@mail.ru>
2022-12-17 13:12:49 +0300

    commit

---
+++ src/test/java/me/viharev/libraries/LibrariesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.viharev.libraries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrariesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
